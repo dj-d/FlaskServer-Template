ef2a45e0de12e308fe6c2e368e700ad191fa8650
Daniele Albanese <daniele.albanese98@gmail.com>
2021-11-14 18:10:12 +0100

    setup.py -> Improved

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import find_packages, setup
 
+requirements = [
+    'flask',
+    'Flask-WTF',
+    'python-dotenv',
+    'waitress'
+]
+
 setup(
     name='AmazonPriceTracker-Backend',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'flask',
-        'Flask-WTF',
-        'python-dotenv',
-        'pytest',
-        'coverage',
-        'waitress'
-    ]
+    install_requires=requirements,
+    extra_require={
+        'dev': [
+            'pytest',
+            'coverage'
+        ]
+    }
 )
